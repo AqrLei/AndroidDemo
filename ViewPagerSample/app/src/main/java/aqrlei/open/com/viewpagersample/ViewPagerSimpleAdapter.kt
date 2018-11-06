cc847f57ad790df16dd39cc2de6a1e343709a504
AqrLei <leilei@guoshujinfu.com>
2018-11-06 14:37:23 +0800

    update

--- ViewPagerSample/app/src/main/java/aqrlei/open/com/viewpagersample/ViewPagerSimpleAdapter.kt
+++ ViewPagerSample/app/src/main/java/aqrlei/open/com/viewpagersample/ViewPagerSimpleAdapter.kt
@@ -1,6 +1,6 @@
 package  aqrlei.open.com.viewpagersample
 
-import android.support.v4.view.PagerAdapter
+import androidx.viewpager.widget.PagerAdapter
 import android.view.View
 import android.view.ViewGroup
 
